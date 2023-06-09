137d1c1326350c45fdfbb43ec4df276f13691512
Noaman <noamangg@gmail.com>
2023-06-09 21:42:04 +0300

    api project

--- GitHub API/main.js
+++ GitHub API/main.js
@@ -1,23 +1,46 @@
 let inputF = document.querySelector('input#input-text');
 let repoContainer = document.querySelector('ul.result-box');
+let flag = true;
 
-
-inputF.addEventListener("keydown", function(e) {
-    if(e.keyCode === 13) {
+inputF.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13 && flag) {
+        console.log(inputF.value);
+        console.log(`check user value = ${checkUser(inputF.value)}`)
+        if(checkUser(inputF.value))
         getRepo(inputF.value);
         inputF.value = "";
     }
 })
 
+function checkUser(username) {
+    let flag = true;
+    fetch(`https://api.github.com/users/${username}/repos`).then((res) => {
+        // flag = (res.status === 200 && res.ok === true);
+        flag = false;
+    })
+    console.log(flag);
+    return flag;
+}
+
 
 function getRepo(username) {
     fetch(`https://api.github.com/users/${username}/repos`).then((res) => {
+        console.log(res, ' outside');
+        if (this.status === 200 && this.ok === true) {
+            return res.json();
+        }
+        else {
+            alert('Enter a valid repo');
+            flag = false;
+            return false;
+        }
         return res.json();
     }).then(repos => {
         // my code here
-        repos.forEach((repo) => {
-            createLi(repo)
-        })
+        if (repos)
+            repos.forEach((repo) => {
+                createLi(repo)
+            })
     })
 }
 
